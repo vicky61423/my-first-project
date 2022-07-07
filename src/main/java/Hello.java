37ff3fce3ea01786c51e84f65ac5b9533b87cee9
VickyTsai <vicky61423@gmail.com>
2022-07-07 18:32:09 +0800

    try test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,7 +13,7 @@ public class Hello extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello from Servlet");
+		out.println("Hello from Servlet test");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
